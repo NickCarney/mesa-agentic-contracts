8d2f5cfc415320205e8d791f6cf03793ec99608e
Nick Carney <nickcarney@Nicks-MacBook-Air.local>
2025-02-05 16:10:56 -0500

    step 1 with console log

--- agentic-contracts/app/api/query_agent/route.js
+++ agentic-contracts/app/api/query_agent/route.js
@@ -1,24 +1,22 @@
+import { NextRequest, NextResponse } from 'next/server';
 import { Pinecone } from "@pinecone-database/pinecone";
-import { give_prompt } from "@/utils/agentkitIntegration";
+//import { give_prompt } from "@/utils/agentkitIntegration";
 import { fetchLawInsider } from "@/lib/fetchLawInsider";
 import { generatePrompt } from "@/lib/generatePrompt";
 
 
-exports.handler = async (event) => {
+export async function POST(req,res) {
     try {
         //1. get data (ipfs link and jurisdiction) from supabase webhook
+        const ipfsLink = req.body.record.ipfs_link;
+        const jurisdiction = req.body.record.jurisdiction;
+        console.log('New contract inserted:',  ipfsLink, jurisdiction );
         //2. parse contract from ipfs link
         //3. query pinecone for similar contracts (context)
         //4. generate prompt for agentkit based on context
         //5. call agentkit to generate new contract and post onchain
         //6.? docusign integration
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-              message: 'Contract processed and posted onchain successfully.',
-              agentkitResult,
-            }),
-          };
+        return NextResponse.json({ ipfs: ipfsLink });
     } catch (error) {
         console.error('Error processing contract:', error);
         return {
@@ -28,3 +26,6 @@ exports.handler = async (event) => {
     }
 
 };
+
+
+
