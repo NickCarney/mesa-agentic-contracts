bf8fdbf05bd0b9ad2c59d61b0541d26b84179a5b
Nick Carney <nickcarney@nicks-air.myfiosgateway.com>
2025-02-08 18:52:35 -0500

    removing unused files

--- agentic-contracts/lib/generatePrompt.ts
+++
@@ -1,9 +0,0 @@
-export const generatePrompt = (userQuery: string, apiContext: string, docContext: string) => `
-You are an expert legal assistant. Here's a relevant legal contract that you will translate:
-
-The document you are using as a template is:
-${docContext}
-
-Please answer the following user query:
-${userQuery}
-`;
